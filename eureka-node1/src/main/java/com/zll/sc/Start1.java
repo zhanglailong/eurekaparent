26b0e312b28ce451b2734e773803d8869f3fbeb9
zhanglailong <1814353637@qq.com>
2021-04-25 22:56:42 +0800

    aaaa

--- eureka-node1/src/main/java/com/zll/sc/Start1.java
+++ eureka-node1/src/main/java/com/zll/sc/Start1.java
@@ -4,10 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+import java.sql.SQLOutput;
+
 @SpringBootApplication
 @EnableEurekaServer
 public class Start1 {
-    public static void main(String[] args) {
+
+    public static void main(String[] args)
+    {
+        System.out.println('1111');
         SpringApplication.run(Start1.class);
     }
 }
